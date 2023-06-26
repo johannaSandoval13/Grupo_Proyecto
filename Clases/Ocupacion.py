440e6916e119ab72002739f9f36a1b1f94b7ebc4
juanes <jamitocastillo@gmail.com>
2023-06-26 11:58:47 -0500

    clases

--- Clases/Ocupacion.py
+++ Clases/Ocupacion.py
@@ -3,3 +3,21 @@ class Ocupacion :
         self.__codigo=codigo
         self.__nombre=nombre
         self.__descripcion=descripcion
+        self.__funciones=[]
+    def getCodigo(self):
+        return self.__codigo
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getFunciones(self):
+        return self.__funciones
+    
+    def setCodigo(self, codigo):
+        self.__codigo = codigo
+        
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+        
+    def agregarFunciones(self, funcion):
+        self.__funciones.append(funcion)
